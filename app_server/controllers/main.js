6729af4ff6196856f6f017ad96ea8a88a8b775d6
Ahmetk9122 <71569624+Ahmetk9122@users.noreply.github.com>
2020-11-15 21:21:54 +0300

    versiyon 2

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 module.exports.index=function(req, res, next) {
-  res.render('index', { title: 'endüstri meslek' });
+  res.render('index', { title: 'Express' });
 }
 
 
